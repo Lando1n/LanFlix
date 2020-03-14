edd874cc149272ea99750480f00bd8f06bb4b9d2
Lando1n <landon-martin@hotmail.com>
2020-03-14 19:39:33 -0400

    Updated preflights (#21)
    * Updated preflights to be passing again

--- notifier/run_tests.sh
+++ notifier/run_tests.sh
@@ -1,7 +1,19 @@
 
+#!/bin/bash
+passed=true
+
 for test_file in $(find src -name 'test_*.py');
 do
     cd $(dirname $test_file)
-    python3 -m pytest $(basename $test_file)
+    test=$(basename $test_file)
+    python3 -m pytest $test
+    if ! [ $? -eq 0 ]; then
+        passed=false
+    fi
     cd -
 done
+
+if [ $passed = false ]; then
+    echo Python Tests Failed!
+    exit -1
+fi
