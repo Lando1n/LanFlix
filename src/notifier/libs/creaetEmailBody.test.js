19cf918537def4a59bd31e5f82c79436c2cb9ed1
landoc409 <landon-martin@hotmail.com>
2020-03-22 17:52:22 -0400

    Add createEmailBody snapshot tests

---
+++ src/notifier/libs/creaetEmailBody.test.js
@@ -0,0 +1,27 @@
+const {createMovieEmailBody, createNewShowEmailBody, createRequestEmailBody, createShowEmailBody} = require('./createEmailBody');
+
+test('Show notification snapshot', () => {
+  const name = 'Atlanta';
+  const body = createShowEmailBody(name);
+  expect(body).toMatchSnapshot();
+});
+
+test('Movie notification snapshot', () => {
+  const name = 'Die Hard';
+  const body = createMovieEmailBody(name);
+  expect(body).toMatchSnapshot();
+});
+
+test('New Show notification snapshot', () => {
+  const name = 'Atlanta';
+  const body = createNewShowEmailBody(name);
+  expect(body).toMatchSnapshot();
+});
+
+test('Request notification snapshot', () => {
+  const name = 'Atlanta';
+  const type = 'show';
+  const user = 'Mark';
+  const body = createRequestEmailBody(name, type, user);
+  expect(body).toMatchSnapshot();
+});
