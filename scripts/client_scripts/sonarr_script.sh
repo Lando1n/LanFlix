a5df42db9396d1a7b02c8d1ee55010bbbac97e75
Lando1n <landon-martin@hotmail.com>
2020-03-23 23:55:05 -0400

    Update sonarr_script.sh

--- scripts/client_scripts/sonarr_script.sh
+++ scripts/client_scripts/sonarr_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "$sonarr_release_episodenumbers" != *"," ]]; then
   echo Episode Notification
-  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01E01." >> $HOME/Downloads/log
+  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01E01."
 else
   echo Season Notification
-  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01." >> $HOME/Downloads/log
+  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01."
 fi
