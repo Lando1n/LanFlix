13761dd99648652f77ee10d832572cbf29adbbb7
Landon <landon-martin@hotmail.com>
2020-03-23 23:15:37 -0400

    Update sonarr script to have decimals after

--- scripts/client_scripts/sonarr_script.sh
+++ scripts/client_scripts/sonarr_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "$sonarr_episodefile_episodecount" == "1" ]]; then
   echo Episode Notification
-  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S${sonarr_episodefile_seasonnumber}E${sonarr_episodefile_episodenumbers}"
+  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S${sonarr_episodefile_seasonnumber}E${sonarr_episodefile_episodenumbers}."
 else
   echo Season Notification
-  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S${sonarr_episodefile_seasonnumber}"
+  node $HOME/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S${sonarr_episodefile_seasonnumber}."
 fi
