223257924958525d8f3088b9dba08a46e265c84f
landoc409 <landon-martin@hotmail.com>
2020-04-20 23:47:32 -0400

    http to https for tmdb api

--- public/src/libs/requests/tmdb.js
+++ public/src/libs/requests/tmdb.js
@@ -11,8 +11,8 @@ let theMovieDb = {};
 
 theMovieDb.common = {
   api_key: "0a2f7317616d7c1426d3a93c19babc97",
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "en-US",
   generateQuery: function(options) {
