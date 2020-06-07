bd3631a2d32c7de29bbed9c27e05cdee3a5f0410
landoc409 <landon-martin@hotmail.com>
2020-06-06 20:05:08 -0400

    Fix sonarr script

--- scripts/client_scripts/sonarr_script.sh
+++ scripts/client_scripts/sonarr_script.sh
@@ -4,5 +4,5 @@ if [[ "$sonarr_release_episodenumbers" != *"," ]]; then
   node /opt/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01E01."
 else
   echo Season Notification
-  node /opt/dev/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01."
+  node /opt/LanFlix/src/notifier/notifier-script.js "$sonarr_series_title.S01."
 fi
