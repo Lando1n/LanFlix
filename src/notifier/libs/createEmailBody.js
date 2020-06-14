7b635f096c5431fb39c7f6b1af8d48f4c554d951
landoc409 <landon-martin@hotmail.com>
2020-06-14 16:59:29 -0400

    Make the other emails nicer too

--- src/notifier/libs/createEmailBody.js
+++ src/notifier/libs/createEmailBody.js
@@ -1,4 +1,9 @@
-function createShowEmailBody(name) {
+function createEmailBody(
+  header,
+  body,
+  footer,
+  align = { header: "center", body: "left", footer: "left" }
+) {
   return `<html>
   <style>
   .table {
@@ -8,22 +13,21 @@ function createShowEmailBody(name) {
   }
   .table td, .table th {
     padding: 8px;
+    text-align: ${align.body};
   }
-  .table tr:nth-child(even){background-color: #f2f2f2;}
   .table th {
     border: 1px solid #ddd;
     padding-top: 12px;
     padding-bottom: 12px;
-    text-align: center;
+    text-align: ${align.header};
     background-color: #4CAF50;
     color: white;
   }
-  
   .table tfoot {
     border: 1px solid #ddd;
     padding-top: 12px;
     padding-bottom: 12px;
-    text-align: left;
+    text-align: ${align.footer};
     background-color: #dcdcdc;
   }
   </style>
@@ -31,99 +35,66 @@ function createShowEmailBody(name) {
   <table class='table'>
     <thead>
       <tr>
-         <th>LanFlix Show Alert: ${name}</th>
+         <th>${header}</th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <td>
-          <br>
-            A new episode of <b>${name}</b> has just been added to the Plex Server.
-          <br>
-          <br>
-            From my understanding, you asked to be notified about this.
-          <br>
-          <p>
-            It should be ready any moment to be streamed!
-          <p> 
+          ${body}
         </td>
       </tr>
     </tbody>
     <tfoot>
       <tr>
-        <td>Don't want this notification? Continue to <a href='https://lanflix.firebaseapp.com'>LanFlix</a> and toggle the subscription!</td>
+        <td>${footer}</td>
       </tr>
     </tfoot>
   </table>
   </body>
   </html>`;
+}
+
+function createShowEmailBody(name) {
+  const header = `LanFlix Show Alert: ${name}`;
+  const body = `
+<br>
+  A new episode of <b>${name}</b> has just been added to the Plex Server.
+<br>
+<br>
+  From my understanding, you asked to be notified about this.
+<br>
+<p>
+  It should be ready any moment to be streamed!
+<p> `;
+  const footer = `Don't want this notification? Go to <a href='https://lanflix.firebaseapp.com'>LanFlix</a> and toggle the subscription!`;
+  return createEmailBody(header, body, footer);
 }
 
 function createMovieEmailBody(name) {
-  return `<html>
-  <style>
-  .table {
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-  }
-  .table td, .table th {
-    padding: 8px;
-  }
-  .table tr:nth-child(even){background-color: #f2f2f2;}
-  .table th {
-    border: 1px solid #ddd;
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: center;
-    background-color: #4CAF50;
-    color: white;
-  }
-  
-  .table tfoot {
-    border: 1px solid #ddd;
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #dcdcdc;
-  }
-  </style>
-  <body>
-  <table class='table'>
-    <thead>
-      <tr>
-         <th>LanFlix Movie Alert: ${name}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>
-          <br>
-            <b>${name}</b> has just been added to the Plex Server.
-          <br>
-          <br>
-            From my understanding, you asked to be notified about this.
-          <br>
-          <p>
-            It should be ready any moment to be streamed!
-          <p> 
-        </td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td>Don't want this notification? Continue to <a href='https://lanflix.firebaseapp.com'>LanFlix</a> and toggle the subscription!</td>
-      </tr>
-    </tfoot>
-  </table>
-  </body>
-  </html>`;
+  const header = `LanFlix Movie Alert: ${name}`;
+  const body = `
+<br>
+  <b>${name}</b> has just been added to the Plex Server.
+<br>
+<br>
+  From my understanding, you asked to be notified about this.
+<br>
+<p>
+  It should be ready any moment to be streamed!
+<p> `;
+  const footer = `Don't want this notification? Go to <a href='https://lanflix.firebaseapp.com'>LanFlix</a> and toggle the subscription!`;
+  return createEmailBody(header, body, footer);
 }
 
 function createNewShowEmailBody(name) {
-  return `'${name}' appears to be a new show on the server, would you like to subscribe?</br>
-<p>
-If so, continue to <a href='lanflix.firebaseapp.com'>LanFlix</a> and toggle the subscription!`;
+  const header = `LanFlix New Show Alert: ${name}`;
+  const body = `
+<br>
+  <b>${name}</b> appears to be a new show on the server, would you like to subscribe?
+</br>`;
+  const footer = `Go to <a href='https://lanflix.firebaseapp.com'>LanFlix</a> and toggle the subscription!`;
+  return createEmailBody(header, body, footer);
 }
 
 function createRequestEmailBody(name, { mediaType, which, id, poster_path }) {
@@ -131,16 +102,18 @@ function createRequestEmailBody(name, { mediaType, which, id, poster_path }) {
     mediaType === "show"
       ? `https://www.themoviedb.org/tv/${id}`
       : `https://www.themoviedb.org/movie/${id}`;
-  const lines = [
-    `<a href='${imageUri}'><h3>${name}</h3></a>`,
-    `<img src="https://image.tmdb.org/t/p/original${poster_path}" style='width:200px;height:300px;'/><br>`,
-  ];
+
+  const header = `New Request: <a href='${imageUri}'><h3>${name}</h3></a>`;
+  const body = `<img src="https://image.tmdb.org/t/p/original${poster_path}" style='width:200px;height:300px;'/><br>`;
+  let footer = ``;
 
   if (mediaType === "show") {
-    lines.push(`Which: ${which}`);
+    footer = `Which: ${which}`;
   }
 
-  return lines.join("</br>");
+  return createEmailBody(header, body, footer, {
+    body: "center",
+  });
 }
 
 module.exports = {
