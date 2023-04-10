80485b23c356967b3557240bec054f4b08e00af7
Lando1n <landon-martin@hotmail.com>
2023-04-09 20:26:37 -0400

    Issue #165: Increase package.json versions on every push (#168)

--- packages/notifier/lanflix-monitor.js
+++ packages/notifier/lanflix-monitor.js
@@ -7,36 +7,42 @@ const firebaseCert = require("../../config/lanflix-firebase-cert.json");
 
 const firebase = new FirebaseHelper(firebaseCert);
 
-firebase.db.collection("email").onSnapshot((snapshot) => {
-  snapshot.docChanges().forEach(async (change) => {
-    try {
-      const data = change.doc.data();
-      console.log(data);
-      switch (change.type) {
-        case "added":
-          const {
-            recipients: bcc,
-            body: html,
-            subject,
-            ...remainingData
-          } = data;
-          await sendEmail({ bcc, html, subject });
+async function startMonitors() {
+  const { version } = require("./package.json");
+  console.log(`Initializing LanFlix Monitor, version: ${version}`);
+  // start monitoring the email queue
+  firebase.db.collection("email").onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach(async (change) => {
+      try {
+        const data = change.doc.data();
+        console.log(data);
+        switch (change.type) {
+          case "added":
+            const {
+              recipients: bcc,
+              body: html,
+              subject,
+              ...remainingData
+            } = data;
+            await sendEmail({ bcc, html, subject });
+            firebase.removeEmailFromQueue(change.doc.id);
+            break;
+          default:
+            console.warn(`No implementation for '${change.type}'`);
+            break;
+        }
+      } catch (e) {
+        if (e.toString().includes("Error: Recipients not set")) {
           firebase.removeEmailFromQueue(change.doc.id);
-          break;
-        default:
-          console.warn(`No implementation for '${change.type}'`);
-          break;
+        }
+        console.error(e);
       }
-    } catch (e) {
-      if (e.toString().includes("Error: Recipients not set")) {
-        firebase.removeEmailFromQueue(change.doc.id);
-      }
-      console.error(e);
-    }
+    });
   });
-});
+  console.log("Email queue monitoring enabled...");
 
-firebase.getAdminEmail("Requests").then((recipients) => {
+  // Start monitoring for requests
+  const recipients = await firebase.getAdminEmail("Requests");
   firebase.db.collection("requests").onSnapshot((snapshot) => {
     snapshot.docChanges().forEach(async (change) => {
       let name;
@@ -75,5 +81,8 @@ firebase.getAdminEmail("Requests").then((recipients) => {
         }
       }
     });
+    console.log("Website request monitoring enabled...");
   });
-});
+}
+
+startMonitors();
